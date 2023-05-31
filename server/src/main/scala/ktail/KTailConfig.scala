efebca3236b2c59852fb59b1e4613154ddab2b75
Jakub Cichy <jakub.cichy@softwaremill.com>
2023-05-31 16:26:30 +0200

    web socket server implementation

--- server/src/main/scala/ktail/KTailConfig.scala
+++ server/src/main/scala/ktail/KTailConfig.scala
@@ -1,17 +1,25 @@
 package ktail
 
-import zio.{ ULayer, ZLayer }
+import zio.*
+import zio.config.*
+import zio.config.magnolia.deriveConfig
+import zio.config.typesafe.*
 
 case class KTailConfig(port: Int, bootstrapServers: List[String], groupId: String, topics: List[String])
 
 object KTailConfig {
-  val live: ULayer[KTailConfig] =
-    ZLayer.succeed(
-      KTailConfig(
-        port = 8080,
-        bootstrapServers = List("localhost:9092"),
-        groupId = "k-tail-server",
-        topics = List("test-topic-1")
-      )
+  private val config: Config[KTailConfig] = deriveConfig[KTailConfig].nested("k-tail")
+
+  val live: Layer[Config.Error, KTailConfig] =
+    ZLayer.fromZIO(
+      ZIO.config[KTailConfig](config).tap { config =>
+        ZIO.logInfo(s"""
+             |k-tail server configuration:
+             |port: ${config.port}
+             |bootstrap-servers: ${config.bootstrapServers.mkString(",")}
+             |group-id: ${config.groupId}
+             |topics: ${config.topics.mkString(",")}
+             |""".stripMargin)
+      }
     )
 }
