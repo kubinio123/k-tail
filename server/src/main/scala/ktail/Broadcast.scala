9ef28abce3109c2f2410ad6671d04c67a6467a70
Jakub Cichy <jakub.cichy@softwaremill.com>
2023-06-30 12:12:36 +0200

    broadcast refactor

--- server/src/main/scala/ktail/Broadcast.scala
+++ server/src/main/scala/ktail/Broadcast.scala
@@ -1,43 +1,62 @@
 package ktail
 
 import zio.*
-import zio.stm.{ STM, TDequeue, THub, TMap }
+import zio.stm.*
 import zio.stream.{ ZSink, ZStream }
 
 trait Broadcast {
   def subscribe(topic: String): Task[TDequeue[Message]] // ZIO[Any, Throwable, TDequeue[Message]]
-  def unsubscribe(topic: String): UIO[Unit] // ZIO[Any, Nothing, Unit]
+  def unsubscribe(topic: String): UIO[Unit]             // ZIO[Any, Nothing, Unit]
 }
 
-case class BroadcastImpl(subscribers: TMap[String, Int], hubs: TMap[String, THub[Message]]) extends Broadcast {
-  override def subscribe(topic: String): Task[TDequeue[Message]] =
-    (for {
-      counter <- subscribers.updateWith(topic) {
+case class BroadcastImpl(
+    subscribers: TMap[String, Int],
+    hubs: TMap[String, THub[Message]]
+) extends Broadcast {
+
+  override def subscribe(topic: String): Task[TDequeue[Message]] = {
+    val subscribe: USTM[(Int, TDequeue[Message])] = for {
+      subscribers <- incrementSubscribers(topic)
+      hub         <- resolveHub(topic)
+      dequeue     <- hub.subscribe
+    } yield (subscribers, dequeue)
+
+    subscribe.commit.flatMap { case (subscribers, dequeue) =>
+      ZIO.logInfo(s"Topic $topic has $subscribers subscriber(s)").as(dequeue)
+    }
+  }
+
+  private def incrementSubscribers(topic: String): USTM[Int] =
+    subscribers
+      .updateWith(topic) {
         case Some(counter) => Some(counter + 1)
         case _             => Some(1)
       }
-      (hub, message) <- hubs.get(topic).flatMap {
-        case Some(hub) => STM.succeed((hub, s"subscribe to $topic topic hub, ${counter.get} subscriber(s)"))
-        case _ =>
-          THub.unbounded[Message].tap(hubs.put(topic, _)).map((_, s"subscribe to topic $topic, hub created"))
-      }
-      dequeue <- hub.subscribe
-    } yield (dequeue, message)).commit.flatMap { case (dequeue, message) => ZIO.logInfo(message).as(dequeue) }
+      .map(_.get)
 
-  override def unsubscribe(topic: String): UIO[Unit] =
-    (for {
-      counter <- subscribers.updateWith(topic) {
-        case Some(1)       => None
-        case Some(counter) => Some(counter - 1)
-        case None          => None
-      }
-      message <- counter match {
-        case Some(counter) =>
-          STM.unit.map(_ => s"unsubscribe from topic $topic, hub is still active, $counter subscriber(s)")
-        case _ =>
-          hubs.delete(topic).map(_ => s"unsubscribe from topic $topic, no more subscribers, hub closed")
-      }
-    } yield message).commit.flatMap(ZIO.logInfo(_))
+  private def resolveHub(topic: String): USTM[THub[Message]] =
+    hubs.get(topic).flatMap {
+      case Some(hub) => STM.succeed(hub)
+      case _         => THub.unbounded[Message].tap(hubs.put(topic, _))
+    }
+
+  override def unsubscribe(topic: String): UIO[Unit] = {
+    val unsubscribe = for {
+      subscribers <- decrementSubscribers(topic)
+      _           <- if (subscribers.isEmpty) hubs.delete(topic) else STM.unit
+    } yield subscribers
+
+    unsubscribe.commit.flatMap(subscribers =>
+      ZIO.logInfo(s"Topic $topic has ${subscribers.getOrElse(0)} subscriber(s)")
+    )
+  }
+
+  private def decrementSubscribers(topic: String): USTM[Option[Int]] =
+    subscribers.updateWith(topic) {
+      case Some(1)       => None
+      case Some(counter) => Some(counter - 1)
+      case None          => None
+    }
 }
 
 object BroadcastImpl {
@@ -59,7 +78,7 @@ object BroadcastImpl {
                   }
                 } yield ()
 
-                ZIO.logTrace(s"publishing message ${message.id}") *> publish.commit
+                publish.commit
               case _ => ZIO.unit
             }
             .run(ZSink.drain)
